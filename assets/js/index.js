876aff5b66fb9853aa3255c9625d21e731bcb3e4
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-06 18:21:46 -0500

    api for today's weather and 5 day forecast

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,14 @@
-let cityList = $("#city-list");
-let cities = [];
-let apiKey = "fc8bffadcdca6a94d021c093eac22797";
+var cityList = $("#city-list");
+var cities = [];
+var apiKey = "fc8bffadcdca6a94d021c093eac22797";
 
 function formatDay(date) {
-  let date = new Date();
+  var date = new Date();
   console.log(date);
-  let month = date.getMonth() + 1;
-  let day = date.getDate();
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
 
-  let dayOutput =
+  var dayOutput =
     date.getFullYear() +
     "/" +
     (month < 10 ? "0" : "") +
@@ -20,31 +20,125 @@ function formatDay(date) {
 }
 
 start();
-
 function start() {
-  let storeCities = JSON.parse(localStorage.getItem("cities"));
-  if (storeCities !== null) {
-    cities = storeCities;
+  var storeCity = JSON.parse(localStorage.getItem("cities"));
+
+  if (storeCity !== null) {
+    cities = storeCity;
   }
-  renderCity();
+  showCity();
 }
 
-function storeCities() {
-  localStorage.setItem("cities", JSON.stringify(cities));
-  // console.log(localStorage);
+function storedCity() {
+  localStorage.setItem("city", JSON.stringify(city));
 }
 
-function renderCity() {
+function showCity() {
   cityList.empty();
+  for (var i = 0; i < cities.length; i++) {
+    var city = cities[i];
 
-  for (let i = 0; i < cities.length; i++) {
-    let city = cities[i];
-
-    let li = $("<li>").text(city);
+    var li = $("<li>").text(city);
     li.attr("id", "listC");
     li.attr("data-city", city);
     li.attr("class", "list-group-item");
-    // console.log(li);
+    console.log(li);
     cityList.prepend(li);
   }
+  if (!city) {
+    return;
+  } else {
+    getWeather(city);
+  }
+}
+
+function getWeather(cityName) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=" +
+    apiKey;
+
+  $("#today-weather").empty();
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    cityTitle = $("<h3>").text(response.name + " " + formatDay());
+    $("#today-weather").append(cityTitle);
+    var TempetureToNum = parseInt((response.main.temp * 9) / 5 - 459);
+    var cityTemperature = $("<p>").text("Tempeture: " + TempetureToNum + " °F");
+    $("#today-weather").append(cityTemperature);
+    var cityWindSpeed = $("<p>").text(
+      "Wind Speed: " + response.wind.speed + " MPH"
+    );
+    $("#today-weather").append(cityWindSpeed);
+
+    var queryURL1 =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      cityName +
+      "&appid=" +
+      apiKey;
+    $.ajax({
+      url: queryURL1,
+      method: "GET",
+    }).then(function (resFiveDay) {
+      $("#boxes").empty();
+      console.log(resFiveDay);
+      for (var i = 0, j = 0; j <= 5; i = i + 6) {
+        var read_date = resFiveDay.list[i].dt;
+        if (resFiveDay.list[i].dt != resFiveDay.list[i + 1].dt) {
+          var fiveDay = $("<div>");
+          fiveDay.attr("class", "col-3 m-2 bg-primary");
+          var d = new Date(0);
+          d.setUTCSeconds(read_date);
+          var date = d;
+          console.log(date);
+          var month = date.getMonth() + 1;
+          var day = date.getDate();
+          var dayOutput =
+            date.getFullYear() +
+            "/" +
+            (month < 10 ? "0" : "") +
+            month +
+            "/" +
+            (day < 10 ? "0" : "") +
+            day;
+
+          var fiveDayH4 = $("<h6>").text(dayOutput);
+          var imgElTag = $("<img>");
+          var conditions = resFiveDay.list[i].weather[0].main;
+          if (conditions === "cloudy") {
+            imgElTag.attr(
+              "src",
+              "https://img.icons8.com/color/48/000000/cloud.png"
+            );
+          } else if (conditions === "sunny") {
+            imgElTag.attr(
+              "src",
+              "https://img.icons8.com/color/48/000000/summer.png"
+            );
+          } else if (conditions === "rainy") {
+            imgElTag.attr(
+              "src",
+              "https://img.icons8.com/color/48/000000/rain.png"
+            );
+          }
+
+          var pTemperatureK = resFiveDay.list[i].main.temp;
+          console.log(conditions);
+          var TempetureToNum = parseInt((pTemperatureK * 9) / 5 - 459);
+          var pTemperature = $("<p>").text(
+            "Tempeture: " + TempetureToNum + " °F"
+          );
+          fiveDay.append(fiveDayH4);
+          fiveDay.append(imgElTag);
+          fiveDay.append(pTemperature);
+          $("#boxes").append(fiveDay);
+          console.log(resFiveDay);
+          j++;
+        }
+      }
+    });
+  });
 }
