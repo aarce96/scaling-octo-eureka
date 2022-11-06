9483429d80c0cf74b6db1d640545cfd7923a743f
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-06 17:38:31 -0500

    start, show and save city

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,8 +9,42 @@ function formatDay(date) {
   let day = date.getDate();
 
   let dayOutput =
-    date.getFullYear() + "/" +
-    (month < 10 ? "0" : "") + month + "/" +
-    (day < 10 ? "0" : "") + day;
+    date.getFullYear() +
+    "/" +
+    (month < 10 ? "0" : "") +
+    month +
+    "/" +
+    (day < 10 ? "0" : "") +
+    day;
   return dayOutput;
-};
+}
+
+start();
+
+function start() {
+  let storeCities = JSON.parse(localStorage.getItem("cities"));
+  if (storeCities !== null) {
+    cities = storeCities;
+  }
+  renderCity();
+}
+
+function storeCities() {
+  localStorage.setItem("cities", JSON.stringify(cities));
+  // console.log(localStorage);
+}
+
+function renderCity() {
+  cityList.empty();
+
+  for (let i = 0; i < cities.length; i++) {
+    let city = cities[i];
+
+    let li = $("<li>").text(city);
+    li.attr("id", "listC");
+    li.attr("data-city", city);
+    li.attr("class", "list-group-item");
+    // console.log(li);
+    cityList.prepend(li);
+  }
+}
